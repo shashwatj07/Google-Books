5ebdef07107b973c7c7c072cae8d0b789abb2fb7
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2020-06-02 12:30:42 +0530

    initial commit

---
+++ app/src/main/java/com/example/googlebooks/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.googlebooks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textView : TextView = findViewById(R.id.a)
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://foo.com"
+
+
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                // Display the first 500 characters of the response string.
+                textView.text = "Response is: ${response.substring(0, 500)}"
+            },
+            Response.ErrorListener { textView.text = "That didn't work!" })
+
+        queue.add(stringRequest)
+    }
+}
