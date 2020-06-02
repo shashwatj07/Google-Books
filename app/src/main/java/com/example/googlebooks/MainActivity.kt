99b43fcf219b4e7b63264dd95f88160250c443ba
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2020-06-02 20:37:12 +0530

    update

--- app/src/main/java/com/example/googlebooks/MainActivity.kt
+++ app/src/main/java/com/example/googlebooks/MainActivity.kt
@@ -1,25 +1,38 @@
 package com.example.googlebooks
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import com.android.volley.Request
+import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var searchButton : Button
+    lateinit var searchQuery : EditText
+    lateinit var textView : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView : TextView = findViewById(R.id.a)
+        textView = findViewById(R.id.a)
+        searchQuery = findViewById(R.id.search_query)
+        searchButton = findViewById(R.id.search_button)
+        var query : String = ""
         val queue = Volley.newRequestQueue(this)
-        //val queue = Volley.newRequestQueue(activity as Context)
+        searchButton.setOnClickListener{
+            query = getQuery()
+            searchRequest(query, queue)
+        }
+    }
 
-        val url = "https://www.googleapis.com/books/v1/volumes?q=search+terms"
-
+    fun searchRequest(query : String, queue : RequestQueue) {
+        val url = "https://www.googleapis.com/books/v1/volumes?q=" + query
         val stringRequest = object:StringRequest(Request.Method.GET, url,
             Response.Listener<String> { response ->
                 // Display the first 500 characters of the response string.
@@ -32,7 +45,12 @@ class MainActivity : AppCompatActivity() {
                 return headers
             }
         }
-
         queue.add(stringRequest)
+    }
+
+    fun getQuery(): String {
+        val query : String = searchQuery.getText().toString()
+        query.replace(' ', '+')
+        return query
     }
 }
